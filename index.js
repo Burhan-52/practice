7672f28798df147dc3471ddf97b7db4c1582426a
Burhanuddin <burhanrampura786@gmail.com>
2023-12-17 11:02:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// import express from 'express';
+
+// const app = express();
+
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+// app.listen(port, () => {
+//   console.log(`app listen on ${port}`);
+// });
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+    - For any other route not defined in the server return 404
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const testFolder = "./file/";
+
+app.get("/files/", (req, res) => {
+  console.log(req.params.file);
+  const allFilesName = [];
+  fs.readdir(testFolder, (err, files) => {
+    files.forEach((file) => {
+      allFilesName.push(file);
+    });
+
+    res.status(200).json({ files });
+  });
+});
+
+app.get("/files/:file", (req, res) => {
+  const { file } = req.params;
+
+  const filePath = path.join(testFolder, file);
+  console.log(filePath);
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+
+  // res.status(200).send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+module.exports = app;
